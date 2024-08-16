45d6f25aee1247a8c0457f8e11e485ed32945120
karen <karenlorenacriscaceres@gmail.com>
2024-08-16 15:23:45 -0500

    feat: :bento: commit 2:se borro el main de mi rama y se creo el de registro de sucursales

---
+++ registrar.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+def abrirArchivo():
+    if os.path.exists("info.json"):
+        with open("info.json", encoding="utf-8") as file:
+            return json.load(file)
+    else:
+        return {"sucursales": []}
+
+def guardarArchivo(data):
+    with open("info.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+def registro_sucursal():
+    sucursal = abrirArchivo()
+    id = int(input("Ingrese el ID del gerente o responsable: "))
+    nombre = input("Ingrese el nombre de la Sucursal: ")
+    direccion = input("Ingrese la dirección de la sucursal: ")
+    telefono = input("Ingrese el número de teléfono: ")
+    celular = input("Ingrese el número de celular: ")
+    correo = input("Ingrese el correo: ")
+
+    sucursal["sucursales"].append(
+        {
+            "id": id,
+            "nombre": nombre,
+            "direccion": direccion,
+            "telefono": telefono,
+            "celular": celular,
+            "correo": correo
+        }
+    )
+
+    guardarArchivo(sucursal)
+
+registro_sucursal()
